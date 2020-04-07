b354ef1f515030709e74c3177ff2780722f6de67
yagyiya <yagyiyaraj@gmail.com>
2020-04-07 23:22:05 +0530

    yag

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print ('hello world')
-print('hello..!!')
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  passwd="mysql123",
+  database="mydatabase"
+)
+mycursor = mydb.cursor()
+mycursor.execute("UPDATE participants SET score='120' where name='anshika'")
+mydb.commit()
+print(mycursor.rowcount,"record affected")
